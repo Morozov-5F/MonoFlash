0f49c66c76d7b98d9f5d695e4c830b6e7a4736ac
Евгений Морозов <zhe3145@yandex.ru>
2015-08-06 16:44:18 +0300

    Added GetBounds() function

--- MonoFlash/Display/Sprite.cs
+++ MonoFlash/Display/Sprite.cs
@@ -40,6 +40,7 @@ namespace MonoFlash.Display
             child.parent = this;
             child.stage = stage;
             child.dispatchEvent(new Event(Event.ADDED_TO_STAGE));
+            GetBounds();
             return true;
         }
 
@@ -57,7 +58,40 @@ namespace MonoFlash.Display
             child.stage = null;
             children.Remove(child);
             child.dispatchEvent(new Event(Event.REMOVED_FROM_STAGE));
+            GetBounds();
             return true;
+        }
+
+        public override Vector4 GetBounds()
+        {
+            float minX = float.MaxValue, maxX = float.MinValue, minY = float.MaxValue, maxY = float.MinValue;
+            foreach(var currentChild in children)
+            {
+                if (currentChild == null)
+                    continue;
+                Vector4 childBounds = currentChild.GetBounds();
+                minX = Math.Min(minX, childBounds.X);
+                maxX = Math.Max(maxX, childBounds.Z);
+                minY = Math.Min(minY, childBounds.Y);
+                maxY = Math.Max(maxY, childBounds.W);
+            }
+            minX += pos.X; maxX += pos.X; minY += pos.Y; maxY += pos.Y;
+            Matrix matrix = new Matrix(new Vector4(minX, minY, 0, 0), new Vector4(maxX, minY, 0, 0),
+                                       new Vector4(minX, maxY, 0, 0), new Vector4(maxX, maxY, 0, 0));
+            matrix *=  Matrix.CreateScale(new Vector3(scale, 1)) * Matrix.CreateRotationZ(MathHelper.ToRadians(rotation)) ;
+            int i;
+            for (i = 1, minX = matrix[0, 0], maxX = matrix[0, 0], minY = matrix[0, 1], maxY = matrix[0, 1]; i < 4; ++i)
+            {
+                var x = matrix[i, 0];
+                minX = Math.Min(x, minX);
+                maxX = Math.Max(x, maxX);
+                var y = matrix[i, 1];
+                minY = Math.Min(y, minY);
+                maxY = Math.Max(y, maxY);
+            }
+            width = (int)Math.Round(maxX - minX, 0);
+            height = (int)Math.Round(maxY - minY, 0);
+            return new Vector4(minX, minY, maxX, maxY);
         }
     }
 }
