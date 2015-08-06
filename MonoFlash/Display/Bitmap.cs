578e9fc588e6e6f4c28a1eec568f5508aeb700b0
Morozov-5F <zhe3145@yandex.ru>
2015-08-06 16:51:30 +0300

    Update Bitmap.cs
    Fixed previous commit issue

--- MonoFlash/Display/Bitmap.cs
+++ MonoFlash/Display/Bitmap.cs
@@ -38,5 +38,29 @@ namespace MonoFlash.Display
 
             base.render(spriteBatch);
         }
+        
+        public override Vector4 GetBounds()
+        {
+            var rect = new Rectangle((int)pos.X, (int)pos.Y, width, height);
+
+            var matrix = new Matrix(new Vector4(rect.X, rect.Y, 0, 0), new Vector4(rect.X + rect.Width, rect.Y, 0, 0),
+                                    new Vector4(rect.X, rect.Y + rect.Height, 0, 0), new Vector4(rect.X + rect.Width, rect.Y + rect.Height, 0, 0));
+            matrix *= Matrix.CreateScale(new Vector3(scale, 1)) * Matrix.CreateRotationZ(MathHelper.ToRadians(rotation)) ;
+            // OH SHI~
+            float minX = matrix[0, 0], maxX = matrix[0, 0], minY = matrix[0, 1], maxY = matrix[0, 1];
+            for (int i = 1; i < 4; ++i)
+            {
+                var x = matrix[i, 0];
+                minX = Math.Min(x, minX);
+                maxX = Math.Max(x, maxX);
+                var y = matrix[i, 1];
+                minY = Math.Min(y, minY);
+                maxY = Math.Max(y, maxY);
+            }
+            //rect = new Rectangle((int)topLeft.Translation.X, (int)topLeft.Translation.Y, (int)(topRight.Translation.X - topLeft.Translation.X), (int)(botLeft.Translation.Y - topLeft.Translation.Y));
+            width = (int)Math.Ceiling(maxX - minX);
+            height = (int)Math.Ceiling(maxY - minY);
+            return new Vector4(minX, minY, maxX, maxY);
+        }
     }
 }
